538b569acbde829b6488ecfe5f267ebf9097c239
Constantin <razvan1598@gmail.com>
2021-11-04 11:48:53 +0100

    Get All Users Web service

---
+++ WebServiceLayer/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServiceLayer.ViewModels;
+
+namespace WebServiceLayer.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+        IDataService _dataService;
+        LinkGenerator _linkGenerator;
+
+        public UserController(IDataService dataService, LinkGenerator linkGenerator)
+        {
+            _dataService = dataService;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _dataService.GetUsers();
+
+            return Ok(users.Select(x => GetUserViewModel(x)));
+        }
+
+        private UserViewModel GetUserViewModel(User user)
+        {
+            return new UserViewModel
+            {
+                Url = _linkGenerator.GetUriByName(HttpContext, nameof(GetUsers), new { user.Id }),
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password
+            };
+        }
+    }
+}
---
+++ WebServiceLayer/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServiceLayer.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
