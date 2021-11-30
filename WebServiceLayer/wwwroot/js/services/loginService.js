1a13427775b8878fa465d42d2451a78e624a771a
Constantin <razvan1598@gmail.com>
2021-11-30 16:52:43 +0100

    started wroking on the frontend of the application: required, ko, jquery setup postman subscribe and publish component skeleton

---
+++ WebServiceLayer/wwwroot/js/services/loginService.js
@@ -0,0 +1,23 @@
+﻿define([], () => {
+
+    let loginUser = (userCredentials, callback) => {
+        let param = {
+            method: "POST",
+            body: JSON.stringify(userCredentials),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        fetch("/api/users/login", param)
+            .then(response => response.json())
+            .then(json => {
+                localStorage.setItem("jwt", json.token);
+                callback(json);
+                // localStorage.getItem("lastname");
+            });
+    };
+
+    return {
+        loginUser
+    }
+});
