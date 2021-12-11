6d29874a0c9714faa2439d0890c5057094172ac9
Albert <turcu.albert98@gmail.com>
2021-12-11 12:48:35 +0100

    fix token auth ui

--- WebServiceLayer/wwwroot/js/viewmodel.js
+++ WebServiceLayer/wwwroot/js/viewmodel.js
@@ -1,14 +1,20 @@
 ﻿define(["knockout", "postman", "searchService"], function (ko, postman, ss) {
-    isUserAuth = localStorage.getItem("jwt")
+    let isTokenExpired = true;
+    const jwt = localStorage.getItem("jwt")
+
+    if (jwt) {
+        isTokenExpired = !(JSON.parse(atob(jwt.split('.')[1])).exp < Date.now())
+    }
 
     let currentView;
     let currentViewParams = ko.observable()
 
-    if (isUserAuth) {
+    if (!isTokenExpired) {
         currentView = ko.observable("dashboard");
     } else {
         document.getElementsByTagName("nav")[0].style.display = "none";
         currentView = ko.observable("login-user");
+        localStorage.removeItem("jwt")
     }
 
     let menuItems = [
@@ -17,14 +23,6 @@
         { title: "Person", component: "person" },
     ];
 
-    let changeContent = menuItem => {
-        if (isUserAuth) {
-            postman.publish("changeView", menuItem.component);
-        } else {
-            alert("Please login!")
-        }
-    };
-
     let userItems = [
         { title: "Profile", component: "#" },
         { title: "Search History", component: "search-history" },
@@ -32,20 +30,25 @@
         { title: "Logout"}
     ];
 
-    let changeUserContent = menuItem => {
-        if (isUserAuth) {
-            if (menuItem.title == "Logout") {
-                logout()
-                return
-            }
-            postman.publish("changeView", menuItem.component);
-        } else {
-            alert("Please login!");
+    let changeContent = menuItem => {
+        if (menuItem.title == "Logout") {
+            logout()
+            return
         }
+        postman.publish("changeView", menuItem.component);
+    };
+
+    let changeUserContent = menuItem => {
+        if (menuItem.title == "Logout") {
+            logout()
+            return
+        }
+        postman.publish("changeView", menuItem.component);
     };
 
     let logout = () => {
         localStorage.removeItem("jwt");
+        isTokenExpired = true;
         location.reload();
     }
 
