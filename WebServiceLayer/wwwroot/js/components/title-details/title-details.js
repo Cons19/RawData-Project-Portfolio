ebaeceb9dc82c8555789fc09387bb7be089ed51f
Constantin <razvan1598@gmail.com>
2021-12-12 17:13:03 +0100

    similar titles

--- WebServiceLayer/wwwroot/js/components/title-details/title-details.js
+++ WebServiceLayer/wwwroot/js/components/title-details/title-details.js
@@ -1,7 +1,9 @@
 ﻿define(["knockout", "titleService", "postman"], function (ko, ts, postman) {
     return function (params) {
         let title = ko.observable();
-
+        let similarTitles = ko.observableArray([]);
+        let url = "api/titles/similar-title/" + params.id;
+        let errorMessage = document.getElementById("error-message");
         title(params);
 
         let back = () => {
@@ -10,9 +12,23 @@
             }, params.currentPage);
         };
 
+        ts.getSimilarTitles(json => {
+            if (json.status === 404) {
+                errorMessage.textContent = "No similar titles were found for this title.";
+                document.getElementById("similarTitles").style.display = "none";
+            } else {
+                errorMessage.textContent = "";
+                document.getElementById("similarTitles").style.display = "block";
+                console.log(json);
+                similarTitles(json);
+            }
+        }, url);
+
+
         return {
             back,
-            title
+            title,
+            similarTitles
         }
     };
 });
--- WebServiceLayer/wwwroot/js/services/titleService.js
+++ WebServiceLayer/wwwroot/js/services/titleService.js
@@ -13,7 +13,21 @@
             });
     };
 
+    let getSimilarTitles = (callback, url) => {
+        fetch(url, {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + localStorage.getItem("jwt")
+            })
+        })
+            .then(response => response.json())
+            .then(json => {
+                callback(json);
+            });
+    };
+
     return {
-        getTitles
+        getTitles,
+        getSimilarTitles
     }
 });
